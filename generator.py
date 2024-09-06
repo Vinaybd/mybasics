b289486ee273252a04529327eff5f91151b623f7
Vinaybd <vinaydacchu26@gmail.com>
2024-09-06 20:42:46 +0530

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,25 @@
+
+def topten():
+     
+    # yield 1
+    # yield 2
+    # yield 3
+    # yield 4
+    n=1
+    while n <= 10:
+        sq = n*n
+        yield sq
+        n += 1
+
+
+
+
+ 
+values=topten()
+
+# print(values.__next__())
+
+# print(values.__next__())
+
+for i  in values:
+    print(i)
