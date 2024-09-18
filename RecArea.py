603ac287dc9cc3e727e660908d049135a94bd999
Vinaybd <vinaydacchu26@gmail.com>
2024-09-18 20:46:38 +0530

    Add files via upload

---
+++ RecArea.py
@@ -0,0 +1,9 @@
+# Area of the rectangle
+
+length=int(input("Enter the length of rectangle:"))
+width=int(input("Enter the width of the rectangle:"))
+
+length=float(length)
+width=float(width)   #this is using just to converting int into float
+area=length*width
+print(f"your rectangle area is {area}")
