d3dfe71a6ecc242e412f104729bbd6c463dba0b6
Vinaybd <vinaydacchu26@gmail.com>
2024-09-19 23:37:21 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,13 @@
+age=int(input("Enter your age"))
+
+if age>100:
+   print("welcome to greatest age")
+elif age>=18:
+   print("your aligible for voating")
+
+elif age<=0:
+   print("you are not born yet")
+
+else:
+   print("your not aligible for voating")
+
