668e2b39fd9b9d3a395716e5b59261381ea90dbd
Vinaybd <vinaydacchu26@gmail.com>
2024-09-19 23:37:39 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,29 @@
+#example 1
+
+# response = input("would you like to have food ? (Y/N)")
+
+# if response == 'Y':
+#     print("Have some food:")
+# else:
+#     print("No food for you!")
+
+
+#example 2
+
+# name = input("enter your name:")
+
+# if name == "":
+#     print("you  did not type your name!")
+# else:
+#     print(f"Hello {name}")
+
+#example 3 using boolean function
+
+for_sale = True
+
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This item is not for sale")
+
+online=True
