15e34ef2360373cd3e417df2e9a94b7a2201c99f
Vinaybd <vinaydacchu26@gmail.com>
2024-09-29 10:50:06 +0530

    Add files via upload

---
+++ shoopingcart.py
@@ -0,0 +1,27 @@
+#shopping card program
+
+foods =[]
+prices=[]
+total=0
+
+while True:
+    food = input("Enter the food to buy(q to quit):")
+    if food.lower()== 'q':
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}:$"))
+        foods.append(food)
+        prices.append(price)
+
+print("---- Your Cart----")
+
+for food in foods:
+        print(food,end="")
+
+for price in prices:
+    total = total + price
+
+print()
+print(f"Your total is:${total}")
+
+
