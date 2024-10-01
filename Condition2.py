6c0648a4c3208697272044c07639d3e7bc3b2b5f
Vinaybd <vinaydacchu26@gmail.com>
2024-10-01 10:23:13 +0530

    Add files via upload

--- Condition2.py
+++ Condition2.py
@@ -1,60 +1,60 @@
 
-# x = 12
+x = 12
 
-# if x==10:
-#     print("you are in the right way:")
-# elif x==12:
-#     print("You are just near")
-# else:
-#     print("you are in the wrong way:")
+if x==10:
+    print("you are in the right way:")
+elif x==12:
+    print("You are just near")
+else:
+    print("you are in the wrong way:")
 
-# signal =input("Enter the color :")
+signal =input("Enter the color :")
 
-# if signal == "red":
-#     print("Stop")
-# elif signal =="yellow":
-#     print("Ready")
-# elif signal == "Green":
-#     print("Go")
-# else:
-#     print("Unknown signal")
+if signal == "red":
+    print("Stop")
+elif signal =="yellow":
+    print("Ready")
+elif signal == "Green":
+    print("Go")
+else:
+    print("Unknown signal")
 
-# attendence=int(input("Enter your attendence percentage :"))
-# is_teacher_friend = False
+attendence=int(input("Enter your attendence percentage :"))
+is_teacher_friend = False
 
-# if attendence >=75 :
-#     print("test")
-# if attendence <=75 and is_teacher_friend :
-#     print("test")
-# if attendence <=75 and is_teacher_friend == False:
-#     print("No test")
+if attendence >=75 :
+    print("test")
+if attendence <=75 and is_teacher_friend :
+    print("test")
+if attendence <=75 and is_teacher_friend == False:
+    print("No test")
 
-#Assignment
+# Assignment
 
-# gender =input("Enter your gender:")
-# age =int(input("Enter your age:"))
+gender =input("Enter your gender:")
+age =int(input("Enter your age:"))
 
-# if gender == "female":
-#     print("Tiket is free")
-# else:
-#     if age<5:
-#         print("Tiket is free")
-#     elif age <=12:
-#         print("Half tiket")
-#     elif age>=60:
-#         print("Seniro citizen half ticket")
-#     else:
-#         print("You have to pay full fare")
+if gender == "female":
+    print("Tiket is free")
+else:
+    if age<5:
+        print("Tiket is free")
+    elif age <=12:
+        print("Half tiket")
+    elif age>=60:
+        print("Seniro citizen half ticket")
+    else:
+        print("You have to pay full fare")
 
-#Assignment
-# Age = int(input("enter your age:"))
-# is_pass = True
-# if Age<5:
-#     print("Free bus ticket ")
-# elif Age>=60 :
-#     print("Senior citizen bus ticket")
-# else:
-#     print("You have to pay full fare")
+# Assignment
+Age = int(input("enter your age:"))
+is_pass = True
+if Age<5:
+    print("Free bus ticket ")
+elif Age>=60 :
+    print("Senior citizen bus ticket")
+else:
+    print("You have to pay full fare")
 
 #Assignment
 
