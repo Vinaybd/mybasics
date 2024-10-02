07f106b83dd99ef3d3a0434fdb0d494d697c685f
Vinaybd <vinaydacchu26@gmail.com>
2024-10-02 14:02:35 +0530

    Add files via upload

---
+++ NumberGuessGame.py
@@ -0,0 +1,36 @@
+# Python number guessing game
+import random
+
+lowest_num =1
+highest_num =100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running =True
+
+print("Python number guessung game")
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+# print(answer)
+
+while is_running:
+
+    guess = input("Enter your guess:")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses +=1
+
+        if guess <lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"Please Select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too Low! Try again")
+        elif guess > answer:
+            print("Too High! Try again")
+        else:
+            print(f"Correct! The answer was {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running = False
+    else:
+        print("Invalid guess")
+        print(f"Please Select a number between {lowest_num} and {highest_num}")
