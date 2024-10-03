ef6403a425445fd5d94bb6d0f1d493427b4515ce
Vinaybd <vinaydacchu26@gmail.com>
2024-10-03 20:18:52 +0530

    Add files via upload

---
+++ RPSGame.py
@@ -0,0 +1,35 @@
+import random
+
+options =("rock","paper","scissors")
+playing = True
+
+while playing:
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter your choice (rock, paper,  scissors): ")
+
+
+    print(f"player: {player}")
+    print(f"computer: {computer}")
+
+    if player == computer:
+        print("It is a tie")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer =="paper":
+        print("You win!")
+    else:
+        print("You lose!")
+
+    if not input("Play again?(y/n):").lower() == "y":
+        playing = False
+
+print("Thanks for playing!")
+
+
+
+
