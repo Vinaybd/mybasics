c9d98c56df3aaf6e0a291806fe6540e79077dcc4
Vinaybd <vinaydacchu26@gmail.com>
2024-10-07 10:20:45 +0530

    Add files via upload

---
+++ KeywordArg.py
@@ -0,0 +1,11 @@
+#keyword arguments = an arguments preceded by an identifier\
+#                    helps with readability
+#                    order of arguments doesn't matter
+
+
+def hello(greeting,title,first,last):
+    print(f"{greeting} {title} {first} {last}")
+
+hello(greeting="Helloo",title="Mr.",last="Squarepants",first="Spongebob")
+
+
