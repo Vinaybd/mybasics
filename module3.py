0d2d97b396088394dd92cf0d48eca09d0391116d
Vinaybd <vinaydacchu26@gmail.com>
2024-10-15 21:39:47 +0530

    Add files via upload

---
+++ module3.py
@@ -0,0 +1,19 @@
+ 
+import module2
+
+result = module2.pi
+result = module2.square(2)
+result = module2.cube(2)
+result = module2.circumference(2)
+result = module2.area(2)
+
+print(result)
+
+
+# my_module.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
