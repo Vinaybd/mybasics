5ca3fff1b3a26861e9e34da7a9bd951bfb8cea26
Vinaybd <vinaydacchu26@gmail.com>
2024-10-17 19:08:54 +0530

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,14 @@
+
+from script1 import *
+
+def favorite_drink(drink):
+    print(f"Your favorite food is {drink}")
+
+def main():
+ print("This is script2")
+ favorite_food("sushi")
+ favorite_drink("Coffee")
+ print('Goodbye!')
+
+if __name__ == '__main__':
+    main()
