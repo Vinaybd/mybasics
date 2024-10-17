e3cbe2826972ced1e0cbc1a5faa5b2814524d1b7
Vinaybd <vinaydacchu26@gmail.com>
2024-10-17 19:08:17 +0530

    Add files via upload

---
+++ ifStatement.py
@@ -0,0 +1,20 @@
+# if__name__ == "__main__": (this script can be imported or run standalone)
+#                           functions and classes in this module can be reused 
+#                            without the main block of code executing
+
+# ex.library = Import library for functionality
+#            When running library directly,display a help page
+
+#Good practice (code is modular,
+                #  helps readability,
+                #  leaves on global variables,
+                #  avoid unintended execution)
+
+#Ex.library = import library for functionality
+#   when running library directly,display a help page
+
+def main():
+        #Your program goes here
+
+  if  __name__ == '__main__':
+        main()
